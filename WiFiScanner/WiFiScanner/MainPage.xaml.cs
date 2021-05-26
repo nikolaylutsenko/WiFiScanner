cf46e43e3cddb903c5786b65ee461a17cebb81fa
Mykola Lutsenko <mykola_lutsenko@epam.com>
2021-05-26 15:08:07 +0300

    Add project files.

---
+++ WiFiScanner/WiFiScanner/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WiFiScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnScanClicked(object sender, EventArgs e)
+        {
+            // todo: begin scan logic
+        }
+
+        void OnStopClicked(object sender, EventArgs e)
+        {
+            // todo: end scan logic
+        }
+    }
+}
